4e96915223c6f98283b38addef1ca38d72e3d6c9
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-07 20:19:17 +0200

    Add files via upload

---
+++ Task01_Old_Books/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exersice05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string lookedBook = Console.ReadLine();
+            int n = 0;
+            string nextBook = " ";
+            while (nextBook != lookedBook)
+            {
+                nextBook = Console.ReadLine();
+                if (nextBook == "No More Books")
+                {
+                    break;
+                }
+                n += 1;
+            }
+            if (nextBook == "No More Books")
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {n} books.");
+            }
+            else
+            {
+                Console.WriteLine($"You checked {n - 1} books and found it.");
+            }
+        }
+    }
+}
