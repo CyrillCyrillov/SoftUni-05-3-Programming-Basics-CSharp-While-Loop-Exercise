715b0690fbddcec05275ab880e71534ba2811edf
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-07 20:21:30 +0200

    Add files via upload

---
+++ Task03_Vacation/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Task03_Vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double neededMoney = double.Parse(Console.ReadLine());
+            double startMoney = double.Parse(Console.ReadLine());
+            int days = 0;
+            int noSavedDaysIndex = 0;
+            double sum = 0;
+            while (noSavedDaysIndex < 5)
+            {
+                string typeOfDay = Console.ReadLine();
+                double moneyPerDay = double.Parse(Console.ReadLine());
+                days += 1;
+                if (typeOfDay == "spend")
+                {
+                    noSavedDaysIndex += 1;
+                    sum -= moneyPerDay;
+                    if (sum < 0)
+                    {
+                        sum = 0;
+                    }
+                }
+                else
+                {
+                    noSavedDaysIndex = 0;
+                    sum += moneyPerDay;
+                }
+                if (sum >= neededMoney)
+                {
+                    Console.WriteLine($"You saved the money for {days} days.");
+                    break;
+                }
+            }
+            if (noSavedDaysIndex == 5)
+            {
+                Console.WriteLine($"You can't save the money.");
+                Console.WriteLine(days);
+            }
+        }
+    }
+}
